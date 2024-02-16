5ded91813fb3bc4232576649351ba6d5e29dc7d7
shlimsady <federicomoret23@gmail.com>
2024-02-16 18:06:28 -0300

    Creating excercise from local

--- src/app.js
+++ src/app.js
@@ -1,11 +1,26 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const reloadButton = document.getElementById("reloadButton");
+  const mainText = document.getElementById("main-text");
+
+  let initialContent = mainText.innerHTML;
+
+  reloadButton.addEventListener("click", function() {
+    mainText.innerHTML = generateDomain();
+  });
 };
+
+function generateDomain() {
+  let pronouns = ["the", "our", "your", "my", "his", "her", "their", "this", "that", "these"];
+  let subjects = ["runner", "rocket", "ocean", "star", "sun", "moon", "planet", "cloud", "flower", "bird"];
+  let verbs = ["running", "flying", "swimming", "shining", "glowing", "soaring", "floating", "dancing", "singing", "growing"];
+  let adverbs = ["quickly", "slowly", "gracefully", "elegantly", "smoothly", "bravely", "carefully", "happily", "loudly", "quietly"];
+  let domains = [".com", ".net", ".edu", ".org", ".io", ".co", ".info", ".biz", ".tv", ".me"];
+
+  let randomDomain = getRandomElement(pronouns) + getRandomElement(subjects) + getRandomElement(verbs) + getRandomElement(adverbs) + getRandomElement(domains);
+
+  return randomDomain;
+}
+
+function getRandomElement(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
