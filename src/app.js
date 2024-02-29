5efa020c9b8ac96590b5367057c109ef66aaab6f
Federico Moret <106476837+shlimsady@users.noreply.github.com>
2024-02-29 12:46:58 +0000

    changes applied

--- src/app.js
+++ src/app.js
@@ -10,13 +10,63 @@ window.onload = function() {
 };
 
 function generateDomain() {
-  let pronouns = ["the", "our", "your", "my", "his", "her", "their", "this", "that", "these"];
-  let subjects = ["runner", "rocket", "ocean", "star", "sun", "moon", "planet", "cloud", "flower", "bird"];
-  let verbs = ["running", "flying", "swimming", "shining", "glowing", "soaring", "floating", "dancing", "singing", "growing"];
-  let adverbs = ["quickly", "slowly", "gracefully", "elegantly", "smoothly", "bravely", "carefully", "happily", "loudly", "quietly"];
-  let domains = [".com", ".net", ".edu", ".org", ".io", ".co", ".info", ".biz", ".tv", ".me"];
+  let pronouns = [
+    "the",
+    "our",
+    "your",
+    "my",
+    "his",
+    "her",
+    "their",
+    "this",
+    "that",
+    "these"
+  ];
+  let subjects = [
+    "runner",
+    "rocket",
+    "ocean",
+    "star",
+    "sun",
+    "moon",
+    "planet",
+    "cloud",
+    "flower",
+    "bird"
+  ];
+  let verbs = [
+    "running",
+    "flying",
+    "swimming",
+    "shining",
+    "glowing",
+    "soaring",
+    "floating",
+    "dancing",
+    "singing",
+    "growing"
+  ];
+  let adverbs = [
+    "quickly",
+    "slowly",
+    "gracefully",
+    "elegantly",
+    "smoothly",
+    "bravely",
+    "carefully",
+    "happily",
+    "loudly",
+    "quietly"
+  ];
+  let domains = [".com", ".net", ".edu", ".org", ".io", ".info"];
 
-  let randomDomain = getRandomElement(pronouns) + getRandomElement(subjects) + getRandomElement(verbs) + getRandomElement(adverbs) + getRandomElement(domains);
+  let randomDomain =
+    "https://" +
+    getRandomElement(pronouns) +
+    getRandomElement(subjects) +
+    getRandomElement(verbs) +
+    getRandomElement(adverbs) +
+    getRandomElement(domains);
 
   return randomDomain;
 }
